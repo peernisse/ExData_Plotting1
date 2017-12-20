a76665162771a8dd8eb64e6ab7f0a81e0f52f933
peernisse <peernisse@gmail.com>
2017-12-19 22:36:30 -0700

    Initial commit of Plot 1 files

---
+++ plot1.R
@@ -0,0 +1,20 @@
+#Read in data (need a folder called "Data" in the wd)
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "./powerData.zip")
+unzip("./powerData.zip")
+pData<-read.table("./household_power_consumption.txt",header = TRUE,sep=";",stringsAsFactors = FALSE)
+head(pData)
+str(pData)
+
+#Fix dates and times
+newdates<-strptime(pData$Date,format="%d/%m/%Y")
+pData$Date<-as.POSIXct(newdates)
+newtimes<-strptime(pData$Time,format = "%T")
+pData$TIME<-as.POSIXct(newtimes)
+
+#Reduce the DF
+pData<-subset(pData,Date == '2007-02-01' | Date == '2007-02-02')
+
+png("plot1.png",width = 480, height = 480, units = "px")
+hist(as.numeric(pData$Global_active_power),col="red",breaks=13,xlab="Global Active Power (kilowatts)",
+     main="Global Active Power")
+dev.off()
